381e8c529f202cb6bf90f4d7bb63a4b1ca8e7fc1
emmyn5600 <emmy66418@gmail.com>
2023-03-21 12:01:26 +0200

    making navbar resp

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,10 @@ import {FaFacebookSquare} from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div className="grid grid-cols-3 gap-4 items-center justify-center text-white bg-secondary-600 p-8 h-48">
-      <div className="place-self-start">
+    <div className="bg-secondary-600 p-8 h-48">
+
+    <div className=" flex  items-center justify-center text-white ">
+      <div className="place-self-start w-12">
         <h1 className="font-bold text-xl ">About us</h1>
         <p className="">
           Our Dabble Lab Talent Accelerator is a good asset for making
@@ -14,7 +16,7 @@ function Footer() {
           development
         </p>
       </div>
-      <div>
+      <div className="w-1/4">
         <h1 className="font-bold text-xl">Quick links</h1>
         <Link href="" className="block">
         Signup
@@ -26,7 +28,7 @@ function Footer() {
         Dabblerlab
         </Link>
       </div>
-      <div className="place-self-start">
+      <div className="place-self-start w-1/4">
         <h1 className="font-bold text-xl">Social media</h1>
 
         <div>
@@ -37,6 +39,8 @@ function Footer() {
         </div>
 
       </div>
+    </div>
+    <p className="text-center text-white">Designed & Built by DELTA Talents</p>
     </div>
   );
 }
